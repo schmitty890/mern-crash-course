7f0dfdc4086b060a8c05de2495698c5fcba9e8b4
Jason Schmitt <jasonschmitt@Jasons-MBP-2.lan>
2025-06-25 00:18:25 -0400

    adding checks for product id

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -36,7 +36,7 @@ export const updateProduct = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res
       .status(404)
-      .json({ success: false, message: 'product not found' });
+      .json({ success: false, message: 'Invalid product id' });
   }
 
   try {
@@ -52,11 +52,16 @@ export const updateProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
   const { id } = req.params;
   console.log(id);
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: 'Invalid product id' });
+  }
   try {
     await Product.findByIdAndDelete(id);
     res.status(200).json({ success: true, message: 'Product deleted' });
   } catch (error) {
     console.log(`error in deleting product: ${error.message}`);
-    res.status(404).json({ success: false, message: 'Product not found' });
+    res.status(500).json({ success: false, message: 'Server error' });
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ import { connectDB } from './config/db.js';
 import ProductRoutes from './routes/product.route.js';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // allows us to accept json data in req.body
 
 app.use('/api/products', ProductRoutes);
 
 // console.log(process.env.MONGO_URI);
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log(`server started on port http://localhost:3000`);
+  console.log(`server started on port http://localhost:${PORT}`);
 });
